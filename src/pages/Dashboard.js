024bfe4171301fef423847dfa783e8537ec4f70a
Ayush Mhaskar <111340278+Ayushh19@users.noreply.github.com>
2025-03-03 15:41:52 +0530

    [Ayush] fix - fixed the width of notes in grid view and removed unecessary code

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,34 +1,4 @@
 
-// import React, { useState } from "react";
-// import { Outlet } from "react-router-dom";
-// import PrimarySearchAppBar from "../components/Header";
-// import Sidebar from "../components/Sidebar";
-
-// const Dashboard = () => {
-//   const [drawerOpen, setDrawerOpen] = useState(false);
-//   const [isListView, setIsListView] = useState(false); // State for list view
-
-//   const toggleDrawer = () => setDrawerOpen(!drawerOpen);
-
-//   // Function to toggle between grid and list views
-//   const toggleView = () => {
-//     setIsListView(!isListView);
-//   };
-
-//   return (
-//     <>
-//       {/* Pass toggleView to Header */}
-//       <PrimarySearchAppBar toggleDrawer={toggleDrawer} toggleView={toggleView} />
-//       <Sidebar open={drawerOpen} onClose={toggleDrawer} />
-//       <div style={{ padding: "20px" }}>
-//         {/* Pass isListView to NoteInput via Outlet context */}
-//         <Outlet context={{ isListView }} />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Dashboard;
 
 // Dashboard.js
 import React, { useState } from "react";
